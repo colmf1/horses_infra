930985e8ace135741354010596aa65ebcce5844d
colmf1 <colmfearon@hotmail.com>
2025-12-18 16:27:02 +0000

    added test s3 bucket

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"      }
+  }
+}
+
+provider "aws" {
+  region = var.region  
+}
+
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "test-bucket" 
+
+  tags = {
+    Name        = "Test Bucket"
+    Environment = "Dev"
+  }
+}
